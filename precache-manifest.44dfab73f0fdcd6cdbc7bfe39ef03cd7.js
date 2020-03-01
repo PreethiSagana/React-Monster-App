db2126ac869e5a7128fdb9d23066fb2ca6ae136e
Preethi Sagana <preethi.sagana@tipotapp.com>
2020-03-01 18:43:43 +0530

    Updates

---
+++ precache-manifest.44dfab73f0fdcd6cdbc7bfe39ef03cd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5683013b752e9bbe35be383f7b9be999",
+    "url": "/React-Monster-App%20/index.html"
+  },
+  {
+    "revision": "dc691b90a3a8919b7355",
+    "url": "/React-Monster-App%20/static/css/main.0230c8c8.chunk.css"
+  },
+  {
+    "revision": "218d8b2942f22442ff46",
+    "url": "/React-Monster-App%20/static/js/2.b306137d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Monster-App%20/static/js/2.b306137d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc691b90a3a8919b7355",
+    "url": "/React-Monster-App%20/static/js/main.83e5e586.chunk.js"
+  },
+  {
+    "revision": "0d23a376f310b4941890",
+    "url": "/React-Monster-App%20/static/js/runtime-main.9931ce5a.js"
+  }
+]);
